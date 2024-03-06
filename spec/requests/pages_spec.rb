6fb2f0c49419b77126d652cd70376b8a9e5d751e
DB Smith <smithwebdd@gmail.com>
2024-03-06 11:18:14 -0500

    [CHORE] Updated request specs

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
   describe "GET /show" do
+    let(:page) { create(:page, :published) }
+
     it "returns http success" do
-      get "/pages/show"
-      expect(response).to have_http_status(:success)
+      get page_path(slug: page.slug)
+      expect(response).to be_successful
     end
   end
-
 end
