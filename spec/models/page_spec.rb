9552559e00f51d636b8bd8aa31541b9b5cc1dc54
DB Smith <smithwebdd@gmail.com>
2024-03-06 08:15:44 -0500

    [CHORE] Updated Pages spec for published scope

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -53,4 +53,20 @@ RSpec.describe Page, type: :model do
       expect(page.slug).to eq('foo-bar-87')
     end
   end
+
+  # Scope testing
+  describe 'scope' do
+    describe 'published' do
+      let(:page1) { create(:page, :published) }
+      let(:page2) { create(:page) }
+
+      before do
+        [ page1, page2 ]
+      end
+
+      it 'returns only published pages' do
+        expect(Page.published).to eq([ page1 ])
+      end
+    end
+  end
 end
