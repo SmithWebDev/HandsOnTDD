1ebdc6b2d8e984d4bae6cb0e242c4689dc30f608
DB Smith <smithwebdd@gmail.com>
2024-03-19 11:44:01 -0400

    [CHORE] Updated routes to show for the year/month route option

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get "pages/show"
-  get "/search",
+  get "/search/:year/:month",
     to: "search#index",
     year: /\d{4}/,
     month: /\d{2}/
+  get "/search", to: "search#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
   get "/page/:slug", to: "pages#show", slug: /[-a-z0-9+]*/, as: :page
