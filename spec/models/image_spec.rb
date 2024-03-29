c7fb38ea589ecb161eefce64a7fc460783ec89e9
DB Smith <smithwebdd@gmail.com>
2024-03-29 01:08:00 -0400

    [FEAT-IMAGES] Installed Active Storage, created Image model and spec to  verify image validation

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+end
