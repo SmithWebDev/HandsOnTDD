0086fc2655896f6b431e266c1d05ebe019158513
DB Smith <smithwebdd@gmail.com>
2024-03-29 01:09:55 -0400

    [FEAT-IMAGES] Created test to ensure valid factory image

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe Image, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'has a valid factory' do
+    it { expect(build(:image)).to be_valid }
+  end
 end
